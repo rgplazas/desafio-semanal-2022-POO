913075da32f2a232b0deaf350bb5c78692fc9eaf
agustinH2001 <agustinlugo2019@gmail.com>
2022-08-17 20:55:56 -0300

    Adicion de JS

--- src/containers/Reto1-JS.js
+++ src/containers/Reto1-JS.js
@@ -1,7 +1,21 @@
 function anagrama (pal1, pal2) {
     pal1 = pal1.toLowerCase()
     pal2 = pal2.toLowerCase()
-    console.log(pal1)
-    console.log(pal2)
+    if ((pal1 == pal2) || (pal1.length != pal2.length)) {
+        console.log(false)
+        console.log(pal1, pal2)
+    } else{
+        var iguales = true
+        pal1array = pal1.split("")
+        pal2array = pal2.split("")
+        pal1array.sort()
+        pal2array.sort()
+        for (var i=0;i<pal1array.length;i++) {
+            if (pal1array[i]!=pal2array[i]) {
+                iguales = false
+            }
+        }
+        console.log(iguales)
+    }
 }
 anagrama("Antes", "Tensa")
