dc93e8f3911035960900c9cc25befcabedcd65c7
Ricardo Gastón Plazas <rgplazas1990@gmail.com>
2022-09-21 12:42:28 -0300

    Fix(Implementacion de firestore)

--- src/context/FirestoreContex.js
+++ src/context/FirestoreContex.js
@@ -1,20 +1,45 @@
-import ContextoFirestore from "./Contexto";
 import { useState } from "react";
-import { collection, addDoc, updateDoc, onSnapshot, deleteDoc, doc, getDoc, getDocs } from "firebase/firestore";
+import ContextoFirestore from "./Contexto";
+import { collection, getDocs, query, orderBy, limit} from "firebase/firestore";
 import { firestore } from "../services/firebase";
 
 export default function FirestoreContex(props) {
 
     const { children } = props;
 
-    const collectionName = "retosMensual";
-
     //Estados de los useState
+    const [estadoSemanal, setEstadoSemanal] = useState([]);
     const [estadoMensual, setEstadoMensual] = useState([]);
+    
+    // coleccion de datos
+    const collectionNameSemanal = "retosSemanal";
+    const collectionNameMensual = "retosMensual";
+
+    //Referencias firebase
+    //Definir una referencia de consulta con el SDK de Firebase
+    const refRetosSemanal = collection(firestore, collectionNameSemanal);
+    const refRetosMensual = collection(firestore, collectionNameMensual);
 
     //query firebase
+    //const queryRetosSemanalCondicional = query(refRetosSemanal,where("habilitado", ">", 0), limit(3));
+    //const queryRetosMensualCondicional = query(refRetosMensual,where("habilitado", ">", 0), limit(1));
+    //const queryRetosSemanalOrdenado = query(refRetosSemanal, orderBy("app", "asc"), limit(3));
+    //const queryRetosMensualOrdenado = query(refRetosMensual, orderBy("app", "desc"), limit(1));
+    const queryRetosSemanalOrdenado = query(refRetosSemanal, orderBy("app", "asc"));
+    const queryRetosMensualOrdenado = query(refRetosMensual, orderBy("app", "asc"));
+
+    const listameRetosSemanal = async () => {
+        const querySnapshot = await getDocs(queryRetosSemanalOrdenado);
+        const docs = [];
+        querySnapshot.forEach((doc) => {
+            docs.push({ ...doc.data(), id: doc.id });
+        });
+        setEstadoSemanal(docs);
+        
+    }
+    
     const listameRetosMensual = async () => {
-        const querySnapshot = await getDocs(collection(firestore, collectionName));
+        const querySnapshot = await getDocs(queryRetosMensualOrdenado);
         const docs = [];
         querySnapshot.forEach((doc) => {
             docs.push({ ...doc.data(), id: doc.id });
@@ -25,7 +50,9 @@ export default function FirestoreContex(props) {
     return (
         <>
             <ContextoFirestore.Provider value={{
+                retosSemanal: estadoSemanal,
                 retosMensual: estadoMensual,
+                listameRetosSemanal,
                 listameRetosMensual
             }}>
                 {children}
--- src/routes/App.js
+++ src/routes/App.js
@@ -7,7 +7,7 @@ import Login from "../components/Login";
 import Registro from "../components/Registro";
 import "../assets/css/App.css";
 import AuthContex from "../context/AuthContex";
-import RtdbContex from "../context/RtdbContex";
+//import RtdbContex from "../context/RtdbContex";
 import FirestoreContex from "../context/FirestoreContex";
 import ProtetecRoute from "../components/ProtetecRoute";
 function App() {
@@ -20,7 +20,7 @@ function App() {
               <Route exact path="/login" element={<Login />}></Route>
               <Route exact path="/registro" element={<Registro />}></Route>
               <Route exact path="/" element={<ProtetecRoute><Home /></ProtetecRoute>}></Route>
-              <Route exact path="/semanales2022" element={<ProtetecRoute><RtdbContex><Semanal /></RtdbContex></ProtetecRoute>}></Route>
+              <Route exact path="/semanales2022" element={<ProtetecRoute><FirestoreContex ><Semanal /></FirestoreContex></ProtetecRoute>}></Route>
               <Route exact path="/mensuales2022" element={<ProtetecRoute><FirestoreContex><Mensual /></FirestoreContex></ProtetecRoute>}></Route>
             </Routes>
           </Layout>
